94e5a2c7c03e9701943d2ce40f7bdb3260a4c802
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-03-21 12:14:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const  num1 = 31 ;
+const num2 = 2;
+
+const multiply = 31 * 2;
+
+const random = 4;
+
+const num3 =  4;
+const num4 =  0;
+
+const mod = num3 - num4;
+const max = 20;
